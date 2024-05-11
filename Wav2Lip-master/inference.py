5e6beef2843c85d83a1123a901f3e28171b112d2
Kaustubh Agarwal <34659241+kaustubhagarwal@users.noreply.github.com>
2024-05-11 19:57:58 -0400

    Update inference.py

--- Wav2Lip-master/inference.py
+++ Wav2Lip-master/inference.py
@@ -216,10 +216,11 @@ def main():
 
 	if not args.audio.endswith('.wav'):
 		print('Extracting raw audio...')
-		command = 'ffmpeg -y -i {} -strict -2 {}'.format(args.audio, 'temp/temp.wav')
 		current_dir = os.path.dirname(os.path.abspath(__file__))
+		total_path = os.path.join(current_dir,'temp/temp.wav')
+		command = 'ffmpeg -y -i {} -strict -2 {}'.format(args.audio, total_path)
 		subprocess.call(command, shell=True)
-		args.audio = os.path.join(current_dir,'temp/temp.wav')
+		args.audio = total_path
 
 	wav = audio.load_wav(args.audio, 16000)
 	mel = audio.melspectrogram(wav)
