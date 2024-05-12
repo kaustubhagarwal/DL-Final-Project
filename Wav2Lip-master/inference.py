d3e9ddfaf09966964d9a2414b17606b289928d61
Kaustubh Agarwal <34659241+kaustubhagarwal@users.noreply.github.com>
2024-05-11 21:23:37 -0400

    Update inference.py

--- Wav2Lip-master/inference.py
+++ Wav2Lip-master/inference.py
@@ -18,7 +18,7 @@ parser.add_argument('--face', type=str,
 parser.add_argument('--audio', type=str, 
 					help='Filepath of video/audio file to use as raw audio source', required=True)
 parser.add_argument('--outfile', type=str, help='Video path to save result. See default for an e.g.', 
-								default='results/result_voice.mp4')
+								default='result_voice.mp4')
 
 parser.add_argument('--static', type=bool, 
 					help='If True, then use only first video frame for inference', default=False)
